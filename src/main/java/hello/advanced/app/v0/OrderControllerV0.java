f3eea5015c54aa59a47d9d2e30206462b2ae0ce6
yoon <leeyt1201@gmail.com>
2024-02-08 22:34:23 +0900

    v0

---
+++ src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,19 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderServiceV0 orderServiceV0;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId, String itemName) {
+        System.out.println(itemName);
+        orderServiceV0.orderItem(itemId);
+        return "ok";
+    }
+}
